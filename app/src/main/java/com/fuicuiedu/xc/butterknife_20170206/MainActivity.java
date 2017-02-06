726d2eb190390e5fb596536d9ee7d3fe3b0fde80
wxcican <wxcican@qq.com>
2017-02-06 16:33:56 +0800

    Initial commit

---
+++ app/src/main/java/com/fuicuiedu/xc/butterknife_20170206/MainActivity.java
@@ -0,0 +1,23 @@
+package com.fuicuiedu.xc.butterknife_20170206;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_tv)
+    TextView tv;//绑定view
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);//绑定到当前activity身上
+
+        tv.setText("大家好！");
+    }
+}
