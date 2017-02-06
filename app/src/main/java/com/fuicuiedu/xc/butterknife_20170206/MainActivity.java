41fbe7ed2f137424a2885cff55e5e83225362559
wxcican <wxcican@qq.com>
2017-02-06 17:41:17 +0800

    butterknife，绑定集合，视图集合操作，点击事件，多个点击事件

--- app/src/main/java/com/fuicuiedu/xc/butterknife_20170206/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/butterknife_20170206/MainActivity.java
@@ -1,16 +1,37 @@
 package com.fuicuiedu.xc.butterknife_20170206;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import butterknife.BindString;
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.main_tv)
-    TextView tv;//绑定view
+//    @BindView(R.id.main_tv1)
+//    TextView tv1;//绑定view
+//    @BindView(R.id.main_tv2)
+//    TextView tv2;//绑定view
+//    @BindView(R.id.main_tv3)
+//    TextView tv3;//绑定view
+
+    //butterknife绑定视图列表
+//    @BindViews({R.id.main_tv1,R.id.main_tv2,R.id.main_tv3})
+//    List<TextView> textViews;
+
+    //资源绑定
+    @BindString(R.string.app_name)
+    String app_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);//绑定到当前activity身上
 
-        tv.setText("大家好！");
+        //单行复制，ctrl + d
+//        tv1.setText("大家好！");
+//        tv2.setText("大家真好！");
+//        tv3.setText("大家特别好！");
+
+        //集体操作视图列表
+//        ButterKnife.apply(textViews, new ButterKnife.Action<TextView>() {
+//            @Override
+//            public void apply(@NonNull TextView view, int index) {
+//                view.setText("面对疾风吧！");
+//            }
+//        });
     }
+
+    //单个控件点击监听事件
+//    @OnClick(R.id.main_tv1)
+//    public void aaa(){
+//        Toast.makeText(this, "点击了Tv1", Toast.LENGTH_SHORT).show();
+//    }
+
+    //多个控件点击监听
+    @OnClick({R.id.main_tv1,R.id.main_tv2,R.id.main_tv3})
+    public void bbb(View view){
+        switch (view.getId()){
+            case R.id.main_tv1:
+                Toast.makeText(this, "点击了Tv1111", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_tv2:
+                Toast.makeText(this, "点击了Tv2222", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_tv3:
+                Toast.makeText(this, "点击了Tv3333", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
+    
 }
